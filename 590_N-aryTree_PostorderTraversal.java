b34ebadcbbc4937b07ed60ba1625ea9ad058bd35
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-02-06 22:58:09 +0600

    N-ary Tree Posteorder Treavesal

---
+++ 590_N-aryTree_PostorderTraversal.java
@@ -0,0 +1,17 @@
+    public List<Integer> postorder(Node root) {
+     LinkedList<Node> stack = new LinkedList<>();   
+    LinkedList<Integer> output_array = new LinkedList<>();
+        
+        if(root == null){
+            return output_array;
+        }
+        stack.add(root);
+        while(!stack.isEmpty()){
+            Node node = stack.pollLast();
+            output_array.addFirst(node.val);
+            for(Node child : node.children){
+                stack.add(child);
+            }
+        }
+        return output_array;
+    }
