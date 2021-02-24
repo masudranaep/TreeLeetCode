99d5ec32b73fd356665bb1b5642e59dd47b7d5fe
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-02-24 22:21:32 +0600

    Find Bottom Left Tree Value

---
+++ 513. Find Bottom Left Tree Value.java
@@ -0,0 +1,14 @@
+   public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            root = queue.poll();
+            if(root.right != null){
+                queue.offer(root.right);
+            }
+            if(root.left != null){
+                queue.offer(root.left);
+            }
+        }
+        return root.val;
+    }
