27d31c2fe3b46ce80c8601ce40cf37925374ce48
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-04-12 07:28:44 +0600

    Maximum Depth of Binary Tree

---
+++ 104. Maximum Depth of Binary Tree.java
@@ -0,0 +1,11 @@
+  public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        int maxDepth = Math.max(leftDepth, rightDepth) + 1;
+        
+        return maxDepth;
+    }
+}
